468f9f3a6db7517bce9f0d6231ba22de8b8baaa2
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-11-07 05:17:25 +0000

    Updating to latest FlutterFlow output.

--- lib/profile/profile_page/profile_page_widget.dart
+++ lib/profile/profile_page/profile_page_widget.dart
@@ -377,7 +377,7 @@ class _ProfilePageWidgetState extends State<ProfilePageWidget> {
                           padding: EdgeInsetsDirectional.fromSTEB(
                               0.0, 16.0, 0.0, 32.0),
                           child: Text(
-                            'Version 2.0.0',
+                            'Version 2.0.1',
                             style: FlutterFlowTheme.of(context).bodyMedium,
                           ),
                         ),
